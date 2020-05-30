9a003103f4e96e0d6eae91059c111af3f98b4637
juhohe <juho.antti.heinonen@gmail.com>
2020-05-30 15:15:00 +0300

    first commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace imgstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
